9b06913c75db0d9d7f1e50f6d2558a6b2324c758
Soulshaker1 <143871322+Soulshaker1@users.noreply.github.com>
2023-11-24 02:16:48 +0300

    Add files via upload

---
+++ project/accounts/forms.py
@@ -0,0 +1,29 @@
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
+
+
+# необходимо убрать класс кастомной формы и переопределить стандартную форму allauth
+# class SignUpForm(UserCreationForm):
+#     email = forms.EmailField(label="Email")
+#     first_name = forms.CharField(label="Имя")
+#     last_name = forms.CharField(label="Фамилия")
+#
+#     class Meta:
+#         model = User
+#         fields = (
+#             "username",
+#             "first_name",
+#             "last_name",
+#             "email",
+#             "password1",
+#             "password2",
+#         )
+
+#автоматически добавляем нового пользователя в группу common:
+class CustomSignupForm(SignupForm):
+    def save(self, request):
+        user = super().save(request)
+        common_users = Group.objects.get(name="common users")
+        user.groups.add(common_users)
+        return user
